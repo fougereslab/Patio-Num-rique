8af25ad24f8b54f4961c672d4cab16077491e33b
Mickaël Descamps <mickael.descamps@mineyou.fr>
2020-08-07 09:48:18 +0200

    amélioration et commentaire sur projet

---
+++ PROGRAMMATION/COMPLEXE/LED/TEST/TestFonctionnementBlancLed/LED_blanc_CRGB.ino
@@ -0,0 +1,32 @@
+//========== Définition =========
+#include <FastLED.h>
+#define NUM_LEDS 300
+
+//========= Définition =========
+CRGBArray<NUM_LEDS> leds;
+int hue;
+
+//========== Initialisation
+void setup() {
+
+  
+  FastLED.clear(); // Initialisation FastLed
+  FastLED.addLeds<NEOPIXEL,6>(leds, NUM_LEDS); // Paramétrage de FastLed
+   CRGB color = CRGB(255,255,255); // Création de la couleur blanche à partir de composantes RGB
+   FastLED.setBrightness(255); // Paramétrage de la luminosité
+  
+
+  // Réglage de toutes les leds en blanc
+ for(int i = 0; i < NUM_LEDS; i++)
+ {  
+   leds[i]= color;
+  }
+
+  FastLED.show(); // Affichage de toutes les leds
+
+  }
+
+void loop()
+{ 
+  
+}
