75d089c90f323f75e4f6a5066328042bcdd8715f
Mickaël <mike.descamps.serv2630@gmail.com>
2021-01-10 23:22:49 +0100

    Adding PythonDef

---
+++ pythonDef.py
@@ -0,0 +1,12 @@
+"""
+Author: Mickaël Descamps - Mineyou
+Email : mickael.descamps@mineyou.fr
+Projet : Patio numérique
+Organisation : FougèresLab
+Desc: Fichier de définitions des constantes communes à tous le projet
+
+pythonDef.py (c) 2021
+Created:  2021-01-10T22:21:48.712Z
+Modified: 2021-01-10T22:22:24.368Z
+"""
+
